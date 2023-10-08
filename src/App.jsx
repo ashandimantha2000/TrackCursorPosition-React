9b8d30489c7cd5b2693b10e8d163293882030a15
Ashan <ashandimanthalk@gmail.com>
2023-10-08 16:43:05 +0530

    add: position tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import ReactDOM from 'react-dom'
+
+
+function useMouse(){
+  const [mousePosition,setMousePosition] = useState({
+    x:null,
+    y:null
+  })
+
+  useEffect(()=>{
+    function handle(e){
+      setMousePosition({
+        x:e.pageX,
+        y:e.pageY
+      })
+    }
+    document.addEventListener("mousemove",handle)
+    return ()=> document.removeEventListener("mousemove",handle)
+
+  })
+  
+  return mousePosition
+
+}
 
 function App() {
+  const {x,y} = useMouse();
   return (
-    <div>App</div>
+    <div>
+      App
+      <p>Mouse X is {x}</p>
+      <p>Mouse Y is {y}</p>
+    </div>
   )
 }
 
